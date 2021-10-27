783f6dd6732f58778237a6e4d4391f4425ddb830
Michael Stallings <stall84@gmail.com>
2021-10-27 07:23:19 -0400

    Saving a preliminary preview version for client. Will be switching to new branch to clean up some tech debt. This commit basically restyled the HeroBackground (images) and the Banner component coloring for better contrast

--- frontend/src/components/HeroBanner/styles.ts
+++ frontend/src/components/HeroBanner/styles.ts
@@ -5,17 +5,23 @@ import tw from 'tailwind.macro';
 export const Banner = styled.section`
   ${tw`flex flex-col justify-start h-full border-b border-indigo-100`};
     p, h1 {
-      color: white;
-      /* mix-blend-mode: multiply; */
+      color: black;
+      /* mix-blend-mode: overlay; */
+      transition: color 1s;
+      
+      &:hover {
+        
+        ${tw`text-lightRed`}
+      }
     }
 `;
 
 export const Content = styled.p`
   ${tw`text-sm sm:text-base mb-1 sm:mb-8`};
-  
+  /* text-shadow: 0.4px 0.4px 0.4px rgba(200,90,87,200); */
 `;
 
 export const Title = styled.h1`
   ${tw`uppercase mb-1 sm:mb-5 text-lg sm:text-3xl w-full text-center`};
-  text-shadow: 1px 1px 1px black
+  text-shadow: 1px 1px 1px rgba(200,90,87,200);
 `;
--- frontend/src/components/ui/Button/styles.ts
+++ frontend/src/components/ui/Button/styles.ts
@@ -9,10 +9,16 @@ export interface StyledProps {
 
 export const Button = motion.custom(styled.button<StyledProps>`
   outline: none !important;
-  ${tw`py-1 px-4 sm:py-2 sm:px-8 rounded-full border border-black text-black hover:bg-lightRed`};
+  ${tw`py-1 px-4 sm:py-2 sm:px-8 rounded-full border border-black text-black`};
 
   ${({ primary }) => (primary ? tw`bg-offWhite` : tw`text-black`)};
 
   ${({ block }) => block && tw`w-full`};
+
+  transition: background-color 1s, transform 1s;
+  & :hover {
+    ${tw`bg-lightRed`}
+    transform: 20% 20%
+  }
 
 `);
