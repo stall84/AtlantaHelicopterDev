0092798677bda265e7d554e865c32133bfae3e7a
Michael Stallings <stall84@gmail.com>
2021-10-30 20:29:25 -0400

    Removed packages.. see changes. 'Tested'

--- frontend/src/helpers/definitions.ts
+++ frontend/src/helpers/definitions.ts
@@ -1,6 +1,6 @@
-import { FluidObject } from 'gatsby-image';
+// import { FluidObject } from 'gatsby-image';
 
-export type ImageSharpFluid = FluidObject | FluidObject[];
+// export type ImageSharpFluid = FluidObject | FluidObject[];
 
 export type ObjectType = Record<string, any>;
 
