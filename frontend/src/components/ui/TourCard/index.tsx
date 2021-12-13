a39bc91f27d0cad0c7420a68eacacebd5a2ce161
Michael Stallings <stall84@gmail.com>
2021-12-13 02:08:06 -0500

    Got TourCards now using css-in-styled-component pure &:hover events for filtering brightness on bg-image of cards, transitioned, added some style text-shadowing, etc to card's content, etc.

--- frontend/src/components/ui/TourCard/index.tsx
+++ frontend/src/components/ui/TourCard/index.tsx
@@ -5,24 +5,8 @@ import * as Styled from './styles';
 import Button from '../Button';
 import { motion } from 'framer-motion';
 
-// interface Props extends Styled.StyledProps {
-//   title: string;
-//   content: React.ReactNode;
-//   icon: IconProps;
-//   linkTo: string;
-// }
 
-// interface NewProps extends Styled.StyledProps {
-//   id: string;
-//   name: string;
-//   description: string;
-//   photoLink?: string;
-//   cancellationPolicy?: string;
-//   price: number;
-//   priceType?: string;
-// }
-
-interface TourCardXola extends Styled.StyledProps {
+interface TourCardXolaProps extends Styled.StyledProps {
   id: string;
   name: string;
   description: string;
@@ -30,28 +14,32 @@ interface TourCardXola extends Styled.StyledProps {
   cancellationPolicy?: string;
   price: number;
   priceType?: string;
+  hovering: boolean;
 }
 
+const hoverHandler = (event: any) => {
+  return event ? true : false;
+}
 
-const TourCard: React.FC<TourCardXola> = ({ id, center, name, description, price, priceType, photoLink, cancellationPolicy }) => (
+const TourCard: React.FC<TourCardXolaProps> = ({ id, center, name, description, price, priceType, photoLink, cancellationPolicy }) => (
 
   <Styled.TourCard center={center} id={id}>
     {/* <img src={`${process.env.GATSBY_IMG_TEST}${photoLink}_723x542.jpg`} /> */}
-    <Styled.Wrapper center={center}>
-      <div style={{ backgroundImage: `url(${process.env.GATSBY_IMG_TEST}${photoLink}_723x542.jpg)`, backgroundPosition: 'center', backgroundSize: 'cover', height: '400px' }}>
-        <Styled.Title>{name}</Styled.Title>
-        <Styled.Content>{description}</Styled.Content>
-        <Styled.Wrapper topPad>
-          <motion.div whileHover={{ scale: 1.20 }} whileTap={{ scale: 1.15 }} >
-            <Styled.priceDiv>
-              <Styled.h3>${price}</Styled.h3>
-            </Styled.priceDiv>
-          </motion.div>
-          <Styled.Link center target="_blank" href={`https://checkout.xola.com/index.html#seller/${process.env.GATSBY_XOLA_SELLER_ID}/experiences/${id}?openExternal=true`}>
-            <Button>Click to book!</Button>
-          </Styled.Link>
-        </Styled.Wrapper>
-      </div>
+    <Styled.Wrapper photoLink={photoLink} onFocus={e => hoverHandler(e)} hovering >
+      {/* <div style={{ backgroundImage: `url(${process.env.GATSBY_IMG_TEST}${photoLink}_723x542.jpg)`, backgroundPosition: 'center', backgroundSize: 'cover', height: '400px' }}> */}
+      <Styled.Title>{name}</Styled.Title>
+      <Styled.Content>{description}</Styled.Content>
+      <Styled.PriceLink topPad>
+        <motion.div whileHover={{ scale: 1.20 }} whileTap={{ scale: 1.15 }} >
+          <Styled.priceDiv>
+            <Styled.h3>${price}</Styled.h3>
+          </Styled.priceDiv>
+        </motion.div>
+        <Styled.Link center target="_blank" href={`https://checkout.xola.com/index.html#seller/${process.env.GATSBY_XOLA_SELLER_ID}/experiences/${id}?openExternal=true`}>
+          <Button>Click to book!</Button>
+        </Styled.Link>
+      </Styled.PriceLink>
+      {/* </div> */}
     </Styled.Wrapper>
   </Styled.TourCard>
 
