0daec3f454067b10714a876f46616c80b24b9798
Michael Stallings <stall84@gmail.com>
2021-12-19 15:45:16 -0500

    Re-styled the HeroBanner component and inner Banner component to rely on tailwind's hover function. Resized and centered text for mobile.

--- frontend/src/components/HeroBanner/styles.ts
+++ frontend/src/components/HeroBanner/styles.ts
@@ -4,29 +4,22 @@ import tw from 'tailwind.macro';
 
 export const Banner = styled.section`
   ${tw`flex flex-col justify-center h-full border-b border-indigo-100`};
-    p, h1 {
-      color: white;
-      /* mix-blend-mode: overlay; */
-      transition: color 1s;
-      
-      &:hover {
-        
-        ${tw`text-darkRed`};
-        text-shadow: 1px 1px 1px black;
-      }
-    }
+    
 `;
 
 export const Content = styled.p`
   font-family: "GoodTimes", monospace;
-  ${tw`text-sm sm:text-base mb-1 sm:mb-8`};
-  /* text-shadow: 0.4px 0.4px 0.4px rgba(200,90,87,200); */
+  ${tw`text-base text-lightRed text-center mb-1 sm:mb-8`};
+  text-shadow: 0.3px 0.3px 0.2px black;
 `;
 
 export const Title = styled.h1`
   font-family: "GoodTimes", monospace;
-  ${tw`uppercase mb-1 sm:mb-5 text-lg sm:text-5xl w-full text-center`};
+  ${tw`uppercase m-1 text-white sm:mb-5 sm:text-5xl w-full text-center`};
+  ${tw`hover:text-darkRed`}
+  ${tw`text-3xl`}
   text-shadow: 1px 1px 1px rgb(200,90,87);
+  transition: color 1s ease;
 `;
 
 export const ButtonDiv = styled.div`
