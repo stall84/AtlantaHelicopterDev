b51879571d5c89701345d729f881c2bf6d32a9ab
Michael Stallings <stall84@gmail.com>
2021-12-24 21:49:07 -0500

    Initial commit of new faq-page

---
+++ frontend/src/pages/faq.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from 'components/Layout';
+import SEO from 'components/SEO';
+
+
+const FaqPage: React.FC = () => {
+
+    return (
+        <Layout>
+            <SEO title="Frequently Asked Questions" />
+            <div>
+                SupFAQQ
+            </div>
+        </Layout>
+
+    )
+}
+
+export default FaqPage;
