cd95e820d3c635c389d4d00fb6fc25be04d8faf7
Michael Stallings <stall84@gmail.com>
2021-12-25 07:20:57 -0500

    Basic structure of FaqAffliliate component done.

---
+++ frontend/src/components/FaqAffiliate/styles.ts
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+import { motion } from 'framer-motion';
+
--- frontend/src/pages/faq.tsx
+++ frontend/src/pages/faq.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import FaqAffiliate from "components/FaqAffiliate";
 
 
 const FaqPage: React.FC = () => {
@@ -8,9 +9,7 @@ const FaqPage: React.FC = () => {
     return (
         <Layout>
             <SEO title="Frequently Asked Questions" />
-            <div>
-                SupFAQQ
-            </div>
+            <FaqAffiliate />
         </Layout>
 
     )
