eee37bb3b0396d0f4c4b8fdbac592ded11794544
Michael Stallings <stall84@gmail.com>
2021-12-27 23:55:19 -0500

    Still building out infra in sls ymls .. permissions

--- api/src/lambdas/graphql.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const aws_wrapper_1 = require("./aws-wrapper");
-exports.handler = aws_wrapper_1.apolloServer.createHandler();
