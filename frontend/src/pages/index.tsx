610377bed55d2672384fa36c400ff8336f38ef41
Michael Stallings <stall84@gmail.com>
2022-01-02 15:52:20 -0500

    Removed Carousel and Testimonial Components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,12 +4,7 @@ import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 import HeroBanner from 'components/HeroBanner';
 import Services from 'components/Services';
-import Testimonials from 'components/Testimonials';
 import ConctactInfo from 'components/ContactInfo';
-
-import { convertToBgImage } from 'gbimage-bridge';
-import BackgroundImage from 'gatsby-background-image';
-import { GatsbyImage } from 'gatsby-plugin-image';
 
 
 const IndexPage: React.FC = () => {
